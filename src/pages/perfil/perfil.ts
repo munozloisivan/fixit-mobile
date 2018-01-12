749d0555321128863c7acce83fd0411db2b7d2b0
munozloisivan <munozloisivan@gmail.com>
2018-01-12 15:50:07 +0100

    comprobaciones perfil

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 import { UsuarioProvider } from "../../providers/usuario/usuario";
 import {EditPerfilPage} from "../edit-perfil/edit-perfil";
+
+
 
 /**
  * Generated class for the PerfilPage page.
@@ -17,15 +19,17 @@ import {EditPerfilPage} from "../edit-perfil/edit-perfil";
 })
 export class PerfilPage {
 
-  usuario: {/*email: string,
-    nombre: string,
-    apellidos: string,
-    alias: string,
-    dni: string,
-    telefono: string,
-    codigoPostal: string,
-    puntos: number,
-    participantes: number*/};
+  usuario: {
+    /*email: string,
+        nombre: string,
+        apellidos: string,
+        alias: string,
+        dni: string,
+        telefono: string,
+        codigoPostal: string,
+        puntos: number,
+        participantes: number*/
+  };
   identity: {};
   email: string;
   nombre: string;
@@ -36,10 +40,15 @@ export class PerfilPage {
   codigoPostal: string;
   puntos: number;
   participantes: number;
+  avisoscreados: any;
+  camposObligatorios: string;
+  camposOptativos: string;
+  msg : string;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private usuarioRest: UsuarioProvider) {
+              private usuarioRest: UsuarioProvider,
+              private alertCtrl: AlertController) {
 
   }
 
@@ -48,6 +57,12 @@ export class PerfilPage {
     this.identity = JSON.parse(localStorage.getItem('identity'));
     console.log(this.identity['_id']);
     this.getUsuarioDetails();
+
+    if (!this.identity['nombre'] || !this.identity['apellidos'] || !this.identity['dni'] ||  !this.identity['alias'] || !this.identity['telefono'] || !this.identity['codigoPostal']) {
+      console.log('necesario completar perfil');
+      this.presentConfirm();
+    }
+
   }
 
   ionViewWillEnter() {
@@ -57,6 +72,8 @@ export class PerfilPage {
   getUsuarioDetails() {
     this.usuarioRest.showUsuario(this.identity['_id']).then((res) => {
       console.log('Usuario:' + JSON.stringify(res));
+      console.log('res a pelo: ' + res);
+      console.log('jsno array: ');
       this.email = res['email'];
       this.nombre = res['nombre'];
       this.apellidos = res['apellidos'];
@@ -66,6 +83,9 @@ export class PerfilPage {
       this.codigoPostal = res['codigoPostal'];
       this.puntos = res['puntos'];
       this.participantes = res['participantes'];
+      this.avisoscreados = this.identity['avisos']['creados'];
+      console.log('iepa tu' + this.avisoscreados);
+      //console.log('avisos creados' + this.avisoscreados['creados']);
       //this.usuario = res;
       //console.log('this.ususario = res' + this.usuario);
       //this.usuario.email = res['email'];
@@ -79,6 +99,79 @@ export class PerfilPage {
     this.navCtrl.push(EditPerfilPage);
   }
 
+  presentConfirm() {
+    let nombre = this.identity['nombre'];
+    let apellidos = this.identity['apellidos'];
+    let dni = this.identity['dni'];
+    let telefono = this.identity['telefono'];
+    let codigoPostal = this.identity['codigoPostal'];
+    let alias = this.identity['alias'];
+
+
+    if (!nombre || !apellidos || !dni) {
+      if ((!nombre && !apellidos && !dni))
+      this.camposObligatorios = '<ul><li>Nombre</li><li>Apellidos</li><li>Dni</li></ul>';
+      else if (!nombre && !apellidos) {
+        this.camposObligatorios = '<ul><li>Nombre</li><li>Apellidos</li></ul>';
+      }
+      else if (!dni) {
+        this.camposObligatorios = '<ul><li>Dni</li></ul>';
+      }
+
+    }
+    if (!telefono || !codigoPostal || !alias) {
+      if (!telefono && !codigoPostal && !alias) {
+        this.camposOptativos = '<ul><li>Teléfono</li><li>Código postal</li><li>Alias</li></ul>';
+      }
+      else if (!telefono && !codigoPostal) {
+        this.camposOptativos = '<ul><li>Teléfono</li><li>Código postal</li></ul>';
+      }
+      else if (!alias) {
+        this.camposOptativos = '<ul><li>Alias</li></ul>';
+      }
+    }
+
+
+      if (!this.camposOptativos) {
+          this.msg = '<p>Para poder utilizar todas las funcionalidades debes completar todos tus datos personales</p> ' +
+                     '<p><b>Campos obligatorios: </b></p>' + this.camposObligatorios;
+      }
+      if (!this.camposObligatorios) {
+          this.msg = '<p>Para poder utilizar todas las funcionalidades debes completar todos tus datos personales</p> ' +
+                     '<p><b>Campos optativos: </b></p>' + this.camposOptativos;
+      }
+
+      if (!this.camposObligatorios && !this.camposOptativos) {
+         this.msg = '<p>Para poder utilizar todas las funcionalidades debes completar todos tus datos personales</p> ' +
+         '<p><b>Campos obligatorios: </b></p>' + this.camposObligatorios +
+         '<p><b>Campos optativos: </b></p>' + this.camposOptativos;
+      }
+
+
+    let alert = this.alertCtrl.create({
+      title: 'Perfil incompleto',
+      message:  this.msg,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Confirm clicked');
+            this.goEditPerfil();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
 
 }
+
