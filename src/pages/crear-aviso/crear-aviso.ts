24137b8f97207a2b321a321bfae7105f187635a3
Dani <danimb93@gmail.com>
2018-01-18 13:17:22 +0100

    Añadir foto al aviso creado

--- src/pages/crear-aviso/crear-aviso.ts
+++ src/pages/crear-aviso/crear-aviso.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Camera } from '@ionic-native/camera';
+import {Camera, CameraOptions} from '@ionic-native/camera';
 import { IonicPage, NavController, ViewController } from 'ionic-angular';
 
 /**
@@ -16,89 +16,25 @@ import { IonicPage, NavController, ViewController } from 'ionic-angular';
   templateUrl: 'crear-aviso.html',
 })
 export class CrearAvisoPage {
-  @ViewChild('fileInput') fileInput;
+  image: string = null;
 
-  isReadyToSave: boolean;
+  constructor(
+    private camera: Camera
+  ) {}
 
-  item: any;
-  base64Image:any;
-
-  form: FormGroup;
-
-  constructor(public navCtrl: NavController, public viewCtrl: ViewController, formBuilder: FormBuilder, public camera: Camera) {
-    this.form = formBuilder.group({
-      profilePic: [''],
-      name: ['', Validators.required],
-      about: ['']
-    });
-
-    // Watch the form for changes, and
-    this.form.valueChanges.subscribe((v) => {
-      this.isReadyToSave = this.form.valid;
-    });
-  }
-
-  ionViewDidLoad() {
-
-  }
-
-  getPicture() {
-    if (Camera['installed']()) {
-      this.camera.getPicture({
-        destinationType: this.camera.DestinationType.DATA_URL,
-        targetWidth: 96,
-        targetHeight: 96
-      }).then((data) => {
-        this.form.patchValue({ 'profilePic': 'data:image/jpg;base64,' + data });
-      }, (err) => {
-        alert('Unable to take photo');
-      })
-    } else {
-      this.fileInput.nativeElement.click();
+  getPicture(){
+    let options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      quality: 100
     }
-  }
-  /*  Acceso a la galeria de imagenes
-  accessGallery(){
-    this.camera.getPicture({
-      sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
-      destinationType: this.camera.DestinationType.DATA_URL
-    }).then((imageData) => {
-      this.base64Image = 'data:image/jpeg;base64,'+imageData;
-    }, (err) => {
-      alert('Unable to take photo');
-      //console.log(err);
-    });
-
-  }
-*/
-  processWebImage(event) {
-    let reader = new FileReader();
-    reader.onload = (readerEvent) => {
-
-      let imageData = (readerEvent.target as any).result;
-      this.form.patchValue({ 'profilePic': imageData });
-    };
-
-    reader.readAsDataURL(event.target.files[0]);
-  }
-
-  getProfileImageStyle() {
-    return 'url(' + this.form.controls['profilePic'].value + ')'
-  }
-
-  /**
-   * The user cancelled, so we dismiss without sending data back.
-   */
-  cancel() {
-    this.viewCtrl.dismiss();
-  }
-
-  /**
-   * The user is done and wants to create the item, so return it
-   * back to the presenter.
-   */
-  done() {
-    if (!this.form.valid) { return; }
-    this.viewCtrl.dismiss(this.form.value);
+    this.camera.getPicture( options )
+      .then(imageData => {
+        this.image = `data:image/jpeg;base64,${imageData}`;
+      })
+      .catch(error =>{
+        console.error( error );
+      });
   }
 }
