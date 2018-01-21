46a61bef652bf743247b6c1a00b79461e3f87925
munozloisivan <munozloisivan@gmail.com>
2018-01-21 14:01:52 +0100

    foto perfil

--- src/pages/edit-perfil/edit-perfil.ts
+++ src/pages/edit-perfil/edit-perfil.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import {AlertController, IonicPage, LoadingController, NavController, NavParams, ToastController} from 'ionic-angular';
 import {UsuarioProvider} from "../../providers/usuario/usuario";
 import {WelcomePage} from "../welcome/welcome";
-
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 /**
  * Generated class for the EditPerfilPage page.
  *
@@ -28,18 +29,29 @@ export class EditPerfilPage {
   puntos: number;
   participantes: number;
 
+
+  //para la foto
+  imageURI:any;
+  imageFileName:any;
+  image: string;
+
+  kkk: any;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private usuarioRest: UsuarioProvider,
               private toastCtrl: ToastController,
               private alertCtrl: AlertController,
-              public loadingCtrl: LoadingController) {
+              public loadingCtrl: LoadingController,
+              private transfer: FileTransfer,
+              private camera: Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditPerfilPage');
     this.identity = JSON.parse(localStorage.getItem('identity'));
     console.log(this.identity['_id']);
+    this.kkk = this.identity['_id'];
     this.getUsuarioDetails();
   }
 
@@ -156,5 +168,51 @@ export class EditPerfilPage {
     });
     loader.present();
   }
+
+  // cambiar FOTO usuario
+  getImagePer() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      encodingType: this.camera.EncodingType.PNG
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.imageURI = imageData;
+      this.uploadFile();
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  uploadFile() {
+    let loader = this.loadingCtrl.create({
+      content: "Uploading..."
+    });
+    loader.present();
+    const fileTransfer: FileTransferObject = this.transfer.create();
+
+    let options: FileUploadOptions = {
+      fileKey: 'image',
+      fileName: 'ionicfile.jpg',
+      chunkedMode: false,
+      mimeType: "multipart/form-data", //Si gitanada NO, que sera que no, ponemos "multipart/form-data" y hay que quitar dicha gitanada del server, pq asi NO, ya hombre ya
+      headers: {}
+    }
+
+    fileTransfer.upload(this.imageURI, 'http://147.83.7.158:80/usuario/image/' + this.identity['_id'], options)
+      .then((data) => {
+        console.log(data+" Uploaded Successfully");
+        this.imageFileName = "http://192.168.0.7:8080/static/images/ionicfile.jpg";
+        loader.dismiss();
+      }, (err) => {
+        console.log(err);
+        loader.dismiss();
+      });
+  }
+
+
+
 
 }
